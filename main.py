8339f4ccdcf1f1687824b187a37097f87e422e54
Kaiqiang2 <11610325@mail.sustc.edu.cn>
2019-01-04 19:38:18 +0800

    merge KAIQIANG

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from PMOEAD import PMOEAD
-from PF import get_pf
+#from PF import get_pf
+from store_result import store_result
+from controller import parallel_run
+import time
+
 if __name__ == '__main__':
-    population, obj = PMOEAD('clean1.txt', 166, 100, 100, 0, 100)
-    print(get_pf(obj))
+    begin_time = time.time()
+    file_name = 'clean1.txt'
+    feature_num = 166
+    population_size = max(100, min(200, feature_num))
+    iteration_num = 100
+
+    begin = 0
+    end = population_size
+    #population, obj = PMOEAD(file_name, feature_num, population_size, iteration_num, begin, end)
+    population, obj = parallel_run(10, 10, 12, file_name, 166, population_size)
+    store_result(obj, file_name, population_size, iteration_num)
+    #get_pf(obj, file_name, population_size, iteration_num)
+    print(time.time()-begin_time)
