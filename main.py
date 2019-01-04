687cd3eab1d47ad34969259ed5c2d74930cceb78
ooozbh <ooozbh@gmail.com>
2019-01-04 20:51:22 +0800

    sth else

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from PMOEAD import PMOEAD
-# from PF import get_pf
+from PF import get_pf
 from store_result import store_result
 from controller import parallel_run
 import time
 
-if __name__ == '__main__':
+
+def main():
+    _parallel = True if (input('Parallel?(y/n):') == 'y') else False
+    _print = True if (input('Print result?(y/n):') == 'y') else False
     begin_time = time.time()
     file_name = 'clean1.txt'
+    directory = './src/datasets/' + file_name
     feature_num = 166
     population_size = max(100, min(200, feature_num))
     iteration_num = 100
-
-    begin = 0
-    end = population_size
-    # population, obj = PMOEAD(file_name, feature_num, population_size, iteration_num, begin, end)
-    population, obj = parallel_run(10, 10, 12, file_name, 166, population_size)
-    store_result(obj, file_name, population_size, iteration_num)
-    # get_pf(obj, file_name, population_size, iteration_num)
+    if not _parallel:
+        population, obj = PMOEAD(directory, feature_num, population_size, iteration_num, 0, population_size)
+    else:
+        population, obj = parallel_run(10, 10, 8, directory, 166, population_size)
+    store_result(obj, file_name, population_size, iteration_num, _parallel)
+    if _print:
+        get_pf(obj, directory, population_size, iteration_num)
     print(time.time() - begin_time)
+
+
+if __name__ == '__main__':
+    main()
--- store_result.py
+++ store_result.py
@@ -1,5 +1,8 @@
-def store_result(obj, file_name, poplation_size, iteration_num):
-    title = '{}-{}-{}'.format(file_name, poplation_size, iteration_num)
+def store_result(obj, file_name, population_size, iteration_num, parallel):
+    para = 'np'
+    if parallel:
+        para = 'p'
+    title = './src/output/{}-popu{}-iter{}-{}'.format(para, population_size, iteration_num, file_name)
     result = open(title, 'w')
     for i in range(len(obj)):
         result.write(str(obj[i][0]))
